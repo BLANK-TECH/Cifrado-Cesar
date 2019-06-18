4d892724e1e17387988baa6a5e6ef6af7b033607
BLANK-TECH <ivonne_1412@hotmail.com>
2019-06-18 15:09:46 -0500

    La funcion Decifra

--- src/cipher.js
+++ src/cipher.js
@@ -3,13 +3,24 @@ window.cipher = {
     let nuevaPosicion = [];
     let cifrada=[];
     let ascii = [];
-      for(i=0;i<textCifra.length;i++){
+      for(let i=0;i<textCifra.length;i++){
         ascii[i]=textCifra.charCodeAt(i);
         nuevaPosicion [i] = ((ascii[i]- 65 + offsetone)%26)+65;
         cifrada += String.fromCharCode(nuevaPosicion[i]);
       }
-      console.log(cifrada)
+      /*console.log(cifrada)*/
       return cifrada;
-  }
-    // ... 
+  },
+  decode: (offsetTwo, textDecifra) => {
+    let nuevaPosicion = [];
+    let decifrada=[];
+    let ascii = [];
+      for(let i=0;i<textDecifra.length;i++){
+        ascii[i]=textDecifra.charCodeAt(i);
+        nuevaPosicion [i] = ((ascii[i]- 65 - offsetTwo)%26)+65;
+        decifrada += String.fromCharCode(nuevaPosicion[i]);
+      }
+      /*console.log(decifrada)*/
+      return decifrada;
+  } 
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,65 @@
-//llamo los elementos necesarios para mi funcion
+function Inicializar (){
+    
+    const cifrar = document.getElementById('img-cifrar');
+    cifrar.addEventListener('click',printCifra);
+
+    
+
+}
+function InicializarInvitado (){
+
+    const decifrar = document.getElementById('img-decifrar');
+    decifrar.addEventListener('click',printDecifra);
+}
+
+const printCifra  = () => {
+    const boxPrint = document.getElementById('saludo');
+    let textCifra = document.getElementById('text-cifra');
+    let offsetone = document.getElementById('off-setone');
+    offsetone  = parseInt(offsetone.value)
+    textCifra = textCifra.value.toUpperCase()
+    const textCifrado = window.cipher.encode(offsetone,textCifra)
+    boxPrint.innerHTML = textCifrado;  
+    } 
+
+    
+    const printDecifra = () => {
+        let textDecifra = document.getElementById('text-decifra');
+        let offsetTwo = document.getElementById('off-setTwo');
+        const boxPrintDecifra = document.getElementById('txt-decifrado');
+        offsetTwo = parseInt(offsetTwo.value)
+        textDecifra = textDecifra.value.toUpperCase()
+        const textDecifrado = window.cipher.decode(offsetTwo,textDecifra)
+        boxPrintDecifra.innerHTML = textDecifrado;
+    }
+    
+
+/*/llamo los elementos necesarios para mi funcion cifrar
 let textCifra = document.getElementById('text-cifra');
 let offsetone = document.getElementById('off-setone');
 const cifrar = document.getElementById('img-cifrar');
 const boxPrint = document.getElementById('saludo');
 
 
-const printCifra  = () => {
-    offsetone  = parseInt(offsetone.value)
-    textCifra = textCifra.value.toUpperCase()
-    const textCifrado = window.cipher.encode(offsetone,textCifra)
-    boxPrint.innerHTML = textCifrado;  
-    }
-      
-    cifrar.addEventListener('click',printCifra) 
+
+
+    cifrar.addEventListener('click',printCifra);
+
+//llamo los elementos necesarios para mi funcion decifrar
+let textDecifra = document.getElementById('text-decifra');
+let offsetTwo = document.getElementById('off-setTwo');
+const decifrar = document.getElementById('img-decifrar');
+const boxPrintDecifra = document.getElementById('txt-decifrado');
+
+const printDecifra = () => {
+    offsetTwo = parseInt(offsetTwo.value)
+    textDecifra = textDecifra.value.toUpperCase()
+    const textDecifrado = window.cipher.decode(offsetTwo,textDecifra)
+    boxPrintDecifra.innerHTML = textDecifrado;
+}
+   
+    decifrar.addEventListener('click',printDecifra);
 
     //button1.addEventListener('click', () => {
 //      window.prueba.perrito()
-    //})
-     
+    //})*/
